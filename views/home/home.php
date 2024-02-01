00f0838e6f386e47391aae3e8c106952470bce40
Ericson Castasus <castasuse@gmail.com>
2024-02-01 17:06:51 +0800

    updated navbar

--- views/home/home.php
+++ views/home/home.php
@@ -8,7 +8,7 @@ $auth = new Authentication();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['logout'])) {
         $auth->logout();
-    } 
+    }
 }
 
 include("/xampp/htdocs/thesis/models/Home.php");
@@ -19,7 +19,7 @@ $home = new Home();
 $survey = $home->getSurvey();
 $stringUtil = new StringUltilities();
 
-if(isset($_POST["delete-action"])) {
+if (isset($_POST["delete-action"])) {
     $result = $home->removeSurvey($survey["id"]);
 
     if ($result) {
@@ -51,13 +51,13 @@ if (empty($_SESSION["username"])) { ?>
         <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="public/assets/carousel/alumni1.jpg" class="d-block w-100" alt="image 1">
+                    <img src="public/assets/carousel/alumni1.jpg" class="carousel-img d-block w-100" alt="image 1">
                 </div>
                 <div class="carousel-item">
-                    <img src="public/assets/carousel/alumni2.jpg" class="d-block w-100" alt="image 2">
+                    <img src="public/assets/carousel/alumni2.jpg" class="carousel-img d-block w-100" alt="image 2">
                 </div>
                 <div class="carousel-item">
-                    <img src="public/assets/carousel/alumni6.jpg" class="d-block w-100" alt="image 3">
+                    <img src="public/assets/carousel/alumni6.jpg" class="carousel-img d-block w-100" alt="image 3">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -70,11 +70,80 @@ if (empty($_SESSION["username"])) { ?>
             </button>
         </div>
     </div>
+    <div class="container-fluid about main-body-padding">
+        <h1 class="text-center mb-5">About</h1>
+        <div>
+            <div class="d-flex flex-column flex-lg-row bg-dark rounded mb-3">
+                <div class="about-section-title border-4 text-white p-5 d-flex align-items-center">
+                    <p>What is LYFJSHS Alumni Tracking System?</p>
+                </div>
+                <div class="p-5 bg-dark-subtle">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at dapibus nibh. In felis nulla,
+                        mattis vestibulum accumsan ac, dictum sed est. Duis scelerisque, libero eget porta viverra, nisi
+                        elit venenatis nunc, vitae sodales enim sapien at nibh.
+                    </p>
+                </div>
+            </div>
+            <div class="d-flex flex-column flex-lg-row bg-dark border-white rounded mb-3">
+                <div class="about-section-title border-4 border-white text-white p-5 d-flex align-items-center">
+                    <p>Contents of LYFJSHS</p>
+                </div>
+                <div class="flex-fill p-5 bg-dark-subtle">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at dapibus nibh. In felis nulla,
+                        mattis vestibulum accumsan ac, dictum sed est. Duis scelerisque, libero eget porta viverra, nisi
+                        elit venenatis nunc, vitae sodales enim sapien at nibh.
+                    </p>
+                </div>
+            </div>
+            <div class="d-flex flex-column flex-lg-row bg-dark border-white rounded">
+                <div class="about-section-title border-4 border-white text-white p-5 d-flex align-items-center">
+                    <p">Benefits</p>
+                </div>
+                <div class="flex-fill p-5 bg-dark-subtle">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at dapibus nibh. In felis nulla,
+                        mattis vestibulum accumsan ac, dictum sed est. Duis scelerisque, libero eget porta viverra, nisi
+                        elit venenatis nunc, vitae sodales enim sapien at nibh.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid text-white contact bg-secondary main-body-padding">
+        <h1 class="mb-5">Contact us</h1>
+        <div style="font-size: 14px;" class="letter-spacing d-flex fw-normal">
+            <div class="flex-fill border-end border-white pe-2 me-3">
+                <div>
+                    <p class="mb-1">
+                        <i class="me-1 fas fa-phone"></i> Phone or Telephone numbers
+                    </p>
+                    <div style="font-size: 12px;">
+                        <p style="margin-bottom: 0.5px;" class="letter-spacing fw-light">+639999999999</p>
+                        <p class="fw-light">+639999999999</p>
+                    </div>
+                </div>
+                <div>
+                    <p class="mb-1 fw-normal"><i class="fas fa-envelope"></i> Email</p>
+                    <p class="fw-light">info@lyfjshs.com</p>
+                </div>
+            </div>
+            <div class="flex-fill fw-light">
+                <a class="link mb-1" href="#"><i class="me-1 fab fa-facebook"></i> Deped Tayo Luis Y. Ferrer Jr SHS - General Trias City</a>
+                <a class="link mb-1" href="#"><i class="me-1 fas fa-globe-asia"></i> LYFJSHS Official Website </a>
+                <p><i class="fas fa-map-marker-alt me-1"></i> South Square Village, Pasong Kawayan 2, General Trias City 4107, Cavite</p>
+            </div>
+        </div>
+    </div>
+    <div class="bg-dark p-3">
+        <p style="font-size: 12px;" class="mb-0 text-light fw-light text-center">&copy Copyright 2024</p>
+    </div>
 <?php } else { ?>
     <div class="dashboard">
         <div class="welcome mb-3">
             <p class="mb-0">Welcome <?php echo $_SESSION["first_name"] . " " . $_SESSION["last_name"]; ?>!</p>
-            <p class="type"><?php echo $_SESSION["type"];?></p>
+            <p class="type"><?php echo $_SESSION["type"]; ?></p>
             <form class="logout" method="post">
                 <input class="btn btn-link" type="submit" name="logout" value="Logout">
             </form>
@@ -83,8 +152,6 @@ if (empty($_SESSION["username"])) { ?>
     </div>
 <?php } ?>
 
-<div class="about"></div>
-<div class="contact"></div>
 
 <!-- Logout User -->
 
--- views/template/navbar.php
+++ views/template/navbar.php
@@ -5,18 +5,38 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="/thesis/home">Home</a></li>
+            <ul class="navbar-nav align-items-center ms-auto mb-2 mb-lg-0">
                 <?php if (empty($_SESSION["username"])) { ?>
+
+                    <li class="nav-item"><a class="nav-link" href="/thesis/home">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                     <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
-                    <li class="nav-item"><a class="nav-link" href="login">Login</a></li>
+                    <li class="nav-item"><a role="button" class="btn btn-outline-dark" href="login">
+                            <i class="fas fa-sign-in-alt me-1"></i> Login</a>
+                    </li>
                 <?php } else { ?>
-                    <li class="nav-item"><a class="nav-link" href="/thesis/user/index">Profile</a></li>
                     <?php if ($_SESSION["type"] == "admin") { ?>
-                        <li class="nav-item"><a class="nav-link" href="/thesis/users">Users</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/thesis/alumni/index">Alumni</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/thesis/contents">Contents</a></li>
+                        <li class="nav-item">
+                            <p class="me-3 mb-0">Hello, <?php echo $_SESSION["first_name"] . " " . $_SESSION["last_name"]; ?></p>
+                        </li>
+                        <li class="nav-item">
+                            <button class="btn btn-light"><i class="fas fa-sign-out-alt"></i></button>
+                        </li>
+                    <?php } else { ?>
+                        <li class="nav-item"><a class="nav-link" href="/thesis/home">News / Announcements / Events</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Gallery</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Survey</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Search Alumni</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Profile
+                            </a>
+                            <ul style="font-size: 14px;" class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#"><i class="fas fa-user-alt"></i> Profile</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="fas fa-cog"></i> Account Setting</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+                            </ul>
+                        </li>
                     <?php } ?>
                 <?php } ?>
             </ul>
